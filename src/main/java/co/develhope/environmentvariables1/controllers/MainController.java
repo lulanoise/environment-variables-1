dcffd21722f84d491148e46c591211446d6fe7c7
lulanoise <lucrezia.rumore98@gmail.com>
2023-05-02 17:16:12 +0200

    sistemato controller perchè ridondante

--- src/main/java/co/develhope/environmentvariables1/controllers/MainController.java
+++ src/main/java/co/develhope/environmentvariables1/controllers/MainController.java
@@ -12,17 +12,15 @@ public class MainController {
     @Autowired
     private Environment environment;
 
-    @Value("myCustomProps.devName")
+    @Value("${myCustomProps.devName}")
     String devName;
 
-    @Value("myCustomProps.authCode")
+    @Value("${myCustomProps.authCode}")
     String authCode;
 
     @GetMapping("")
     public String getProperties (){
-        String prop1 = environment.getProperty("myCustomProps.devName");
-        String prop2 = environment.getProperty("myCustomProps.authCode");
-        return "Hello! " + prop1 + " " + prop2;
+        return "Hello! " + devName + " " + authCode;
     }
 
 }
